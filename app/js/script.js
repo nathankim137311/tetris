2584ffd7cd3ea7d5c9be6f4593c2b7fd68b40a4c
web-dev <nathankim137311@gmail.com>
2021-09-20 00:18:47 -0700

    added an event listener to the up and down arrow keys

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,9 @@
 console.log('hello everybody'); 
 // variables
 const gameArea = document.getElementById('game-container');
+const divs = document.querySelectorAll('.game-container div')
+
+
 const grid = Array.from(document.querySelectorAll('.game-container div')); 
 const start = document.getElementById('start');
 const rows = 20; 
@@ -8,6 +11,7 @@ const rows = 20;
 start.addEventListener('click', () => {
   let gridDimensions = prompt('How many rows do you want?', 20);
   let rows = parseInt(gridDimensions);
+  clearGrid(gameArea);
   startGame(rows); 
 })
 
@@ -27,6 +31,12 @@ function createGrid (rows) {
     i++; 
   }
  }
+}
+
+const clearGrid = (parent) => {
+  while(parent.firstChild) {
+    parent.removeChild(parent.firstChild); 
+  }
 }
 
 // Tetromino blocks
@@ -81,51 +91,87 @@ const iBlock = [
 
 const tetrominos = [tBlock, lBlock, jBlock, oBlock, sBlock, zBlock, iBlock]
 
-let currentPosition = 3; 
-// let current = tetrominos[0][0];  
+let currentPosition = 3;  
 
 // random shape function 
 function randomShape () {
   shape = tetrominos[Math.floor(Math.random() * tetrominos.length)];
   draw(shape); 
+  return shape;
 }
 
-// adds classlist and adds styling to random tetromino
+// adds classlist and adds color to random tetromino
 function draw() {
   if (shape === tetrominos[0]) {
    shape = tBlock; 
    shape[Math.floor(Math.random() * 3)].forEach(index => {
       grid[currentPosition + index].classList.add('t-block');
       });
+      return shape;
   } else if (shape === tetrominos[1]) {
     shape = lBlock; 
     shape[Math.floor(Math.random() * 3)].forEach(index => {
       grid[currentPosition + index].classList.add('l-block');
       });
+      return shape;
   } else if (shape === tetrominos[2]) {
     shape = jBlock; 
     shape[Math.floor(Math.random() * 3)].forEach(index => {
       grid[currentPosition + index].classList.add('j-block');
       });
+      return shape;
   } else if (shape === tetrominos[3]) {
     shape = oBlock; 
     shape[Math.floor(Math.random() * 3)].forEach(index => {
       grid[currentPosition + index].classList.add('o-block');
       });
+      return shape;
   } else if (shape === tetrominos[4]) {
     shape = sBlock; 
     shape[Math.floor(Math.random() * 3)].forEach(index => {
       grid[currentPosition + index].classList.add('s-block');
       });
+      return shape;
   } else if (shape === tetrominos[5]) {
     shape = zBlock;
     shape[Math.floor(Math.random() * 3)].forEach(index => {
       grid[currentPosition + index].classList.add('z-block');
       }); 
+      return shape;
   } else if (shape === tetrominos[6]) {
     shape = iBlock; 
     shape[Math.floor(Math.random() * 3)].forEach(index => {
       grid[currentPosition + index].classList.add('i-block');
       });
+      return shape;
   }
 }
+
+// keys rotate tetromino 
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowUp') {
+    let rotations = draw()[count];
+    count++
+    if (count === rotations.length) {
+      count = 0; 
+    }
+  } else if (e.key === 'ArrowDown') {
+    draw()[0 - 1];
+  }
+});
+
+
+
+//function cycleRotations (x) {
+//  let = rotations = draw()[count];
+//  console.log(rotations); 
+//  count++;
+//  if (count === rotations.length) {
+//    count = 0; 
+//  }
+//}
+
+//function nextRotation () {
+//  rotations[index]; 
+//  index = (index + 1) % (rotations.length); 
+//}
