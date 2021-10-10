2a96e6ee67d198cf99cd46443ee0641ae9f7c6df
web-dev <nathankim137311@gmail.com>
2021-10-09 18:21:46 -0700

    added addClassTaken function, removed random rotation functionality and added freeze function. Random tetrominos now fall

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,14 @@
 // variables
 const gameArea = document.getElementById('game-container');
-const divs = document.querySelectorAll('.game-container div')
-const grid = Array.from(document.querySelectorAll('.game-container div')); 
+let grid = Array.from(document.querySelectorAll('.game-container div')); 
 const startBtn = document.getElementById('start');
 
 // grid dimensions
-const rows = 20; 
+const rows = 22; 
 const columns = 10; 
 createGrid();
-createDivs(); 
+addClassTaken(); 
+console.log(grid);
 
 // grid 
 function createGrid () {
@@ -22,17 +22,25 @@ function createGrid () {
   }
 }
 
-// adds 20 divs with class taken to the end of array
-function createDivs() {
-  let i = 0; 
-  while (i < 20) {
-    let cell = gameArea.appendChild(document.createElement('div'));
-    let takenDivs = cell.classList.add('taken');
-    grid.push(takenDivs) 
+// adds class 'taken' to the last 20 divs of array
+function addClassTaken() {
+  let i = 200;
+  while (i < grid.length) {
+    grid[i].classList.add('taken'); 
     i++;
   }
 }
 
+// adds 20 divs with class taken to the end of array
+//function createDivs() {
+//  let i = 0; 
+//  while (i < 20) {
+//    let cell = gameArea.appendChild(document.createElement('div'));
+//    let takenDivs = cell.classList.add('taken');
+//    grid.push(takenDivs) 
+//    i++;
+//  }
+//}
 
 // Tetromino blocks
 const tBlock = [
@@ -92,66 +100,59 @@ let tetromino = {
 // tetrominos array 
 const tetrominos = [tBlock, lBlock, jBlock, oBlock, sBlock, zBlock, iBlock];
 
-const randomTetromino = createRandomTetromino();
-let currentBlock = randomTetromino[0];
-let currentRotation = randomTetromino[2];
+let currentBlock = createRandomTetromino();
+let currentRotation = 0;
 let currentTetromino = currentBlock[currentRotation]
 
 // creates random tetromino
 function createRandomTetromino() {
   let randomBlock = Math.floor(Math.random() * tetrominos.length); 
-  let currentRotation = Math.floor(Math.random() * 4); 
-  return [tetrominos[randomBlock], tetrominos[randomBlock][currentRotation], currentRotation];
+  return tetrominos[randomBlock];
 }
 
 // colors the tetromino 
 function draw() {
-  let i = 0;
-  while(i < 4) {
     switch (currentTetromino) {
-      case tBlock[i]:
+      case tBlock[0]:
         currentTetromino.forEach(square => {
         grid[tetromino.currentX + tetromino.currentY + square].classList.add('t-block')});
         break; 
-      case lBlock[i]:
+      case lBlock[0]:
         currentTetromino.forEach(square => {
         grid[tetromino.currentX + tetromino.currentY + square].classList.add('l-block')});
         break; 
-      case jBlock[i]:
+      case jBlock[0]:
         currentTetromino.forEach(square => {
         grid[tetromino.currentX + tetromino.currentY + square].classList.add('j-block')});
         break; 
-      case oBlock[i]:
+      case oBlock[0]:
         currentTetromino.forEach(square => {
         grid[tetromino.currentX + tetromino.currentY + square].classList.add('o-block')});
         break; 
-      case sBlock[i]:
+      case sBlock[0]:
         currentTetromino.forEach(square => {
         grid[tetromino.currentX + tetromino.currentY + square].classList.add('s-block')});
         break; 
-      case zBlock[i]:
+      case zBlock[0]:
         currentTetromino.forEach(square => {
         grid[tetromino.currentX + tetromino.currentY + square].classList.add('z-block')});
         break; 
-      case iBlock[i]:
+      case iBlock[0]:
         currentTetromino.forEach(square => {
         grid[tetromino.currentX + tetromino.currentY + square].classList.add('i-block')});
         break; 
     }
-    i++; 
-  }
 }
 
 startBtn.addEventListener('click', () => {
-  erase(); 
   createRandomTetromino();
   draw();
-  const timerID = setInterval(moveDown, 1000)
+  const timerID = setInterval(moveDown, 500)
 });
 
 // erase the tetromino
 function erase() {
-  for (let i = 0; i < grid.length; i++) {
+  for(let i = 0; i < grid.length; i++) {
     grid[i].classList.remove('t-block', 'l-block', 'j-block', 'o-block', 's-block', 'z-block', 'i-block');
   }
 }
@@ -159,7 +160,6 @@ function erase() {
 // tetromino controls 
 window.addEventListener('keydown', (e) => {
   if (e.key === 'ArrowUp') {
-    console.log('ass')
     erase();
     rotateRight();
     draw();
@@ -180,7 +180,7 @@ window.addEventListener('keydown', (e) => {
 
 // rotate tetromino clockwise
 function rotateRight () {
-  currentTetromino = currentBlock[currentRotation++ % 4];
+  currentTetromino = currentRotation++ % 4;
   if (currentRotation > 3) {
     currentRotation = 0;
   }
@@ -189,7 +189,7 @@ function rotateRight () {
 
 // rotate tetromino counter-clockwise
 function rotateLeft () {
-  currentTetromino = currentBlock[currentRotation--];
+  currentTetromino = currentRotation--;
   if (currentRotation < 0) {
     currentRotation = 3;
   }
@@ -200,22 +200,20 @@ function rotateLeft () {
 function moveDown() {
   erase();  
   tetromino.currentY += 10; 
-  if (tetromino.currentY >= 179) {
-    currentTetromino.forEach(index => grid[tetromino.currentX + index].classList.add('taken')); 
-    clearInterval(timerID);  
-  }
   draw(); 
   freeze(); 
 }
 
 //freeze function 
 function freeze() {
-  if(currentTetromino.some(index => grid[tetromino.currentX + index + 10].classList.contains('taken'))) {
-    currentTetromino.forEach(index => grid[tetromino.currentX + index].classList.add('taken')); 
+  if(currentTetromino.some(index => grid[tetromino.currentX + tetromino.currentY + index + 10].classList.contains('taken'))) {
+    console.log('freeze!');
+    currentTetromino.forEach(square => grid[tetromino.currentX + square].classList.add('taken')); 
     // start a new tetromino falling
-    // random = Math.floor(Math.random() * theTetrominoes.length)
-    // current = theTetrominoes[random][currentRotation];
-    // currentPosition = 4
-    // draw(); 
+    let random = Math.floor(Math.random() * tetrominos.length)
+    currentTetromino = tetrominos[random][currentRotation];
+    tetromino.currentX = 3;
+    tetromino.currentY = 10; 
+    draw(); 
   }
 }
